145db95769553a10d4f780d06268a416b1b8340f
Thanh <thanhz@hotmail.com>
2021-04-22 01:02:39 +0100

    Refactored ExpenseControllerTestSpec to use Junit 5

--- src/test/java/io/github/thanhz/expensetracker/ExpenseControllerTestSpec.java
+++ src/test/java/io/github/thanhz/expensetracker/ExpenseControllerTestSpec.java
@@ -3,13 +3,12 @@ package io.github.thanhz.expensetracker;
 import io.github.thanhz.expensetracker.controller.ExpenseController;
 import io.github.thanhz.expensetracker.model.Expense;
 import io.github.thanhz.expensetracker.service.ExpenseService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -22,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+
 @WebMvcTest(ExpenseController.class)
 public class ExpenseControllerTestSpec {
 
@@ -32,20 +31,35 @@ public class ExpenseControllerTestSpec {
     @MockBean
     private ExpenseService expenseService;
 
-    @Test
-    public void givenExpenses_whenGetExpenses_thenReturnJsonArray()
-            throws Exception {
+    List<Expense> expenseList;
 
+    @BeforeEach
+    public void setup() {
         Expense expense1 = new Expense("expense1", "test", 100.0);
         Expense expense2 = new Expense("expense1", "test", 200.0);
-        List<Expense> expenseList = Arrays.asList(expense1, expense2);
+        expenseList = Arrays.asList(expense1, expense2);
 
-        given(expenseService.getExpenses()).willReturn(expenseList);
+        given(expenseService.getExpenses())
+                .willReturn(expenseList);
 
+        given(expenseService.getTotalExpense())
+                .willReturn(300D);
+    }
+
+    @Test
+    public void givenExpenses_whenGetExpenses_thenReturnJsonArray() throws Exception {
         mvc.perform(get("/expense")
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)))
-                .andExpect(jsonPath("$[0].id", is(expense1.getId())));
+                .andExpect(jsonPath("$[0].id", is(expenseList.get(0).getId())));
+    }
+
+    @Test
+    public void givenExpenses_whenGetTotalExpenses_thenReturnTotalCost() throws Exception {
+        mvc.perform(get("/expense/total")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", is(300D)));
     }
 }
