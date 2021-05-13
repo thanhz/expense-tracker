f93d0d6a0ebb6b7f0abdb995abcdc408794c5fa4
Thanh <thanhz@hotmail.com>
2021-05-13 01:03:43 +0100

    Call expense endpoint and display some data

--- src/frontend/src/components/Orders.js
+++ src/frontend/src/components/Orders.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Link from '@material-ui/core/Link';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
@@ -31,38 +31,46 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 export default function Orders() {
-  const classes = useStyles();
-  return (
-    <React.Fragment>
-      <Title>Recent Orders</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
-            <TableCell>Ship To</TableCell>
-            <TableCell>Payment Method</TableCell>
-            <TableCell align="right">Sale Amount</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.shipTo}</TableCell>
-              <TableCell>{row.paymentMethod}</TableCell>
-              <TableCell align="right">{row.amount}</TableCell>
+  const [count, setCount] = useState([],);
+
+  useEffect(() => {    
+    fetch(`http://localhost:8080/expense`)
+    .then(response => response.json()
+    .then(data => setCount(data)))
+    .catch(e => console.log(e))
+    });
+
+    const classes = useStyles();
+    return (
+      <React.Fragment>
+        <Title>Recent Expenses</Title>
+        <Table size="small">
+          <TableHead>
+            <TableRow>
+              <TableCell>Date</TableCell>
+              <TableCell>Name</TableCell>
+              <TableCell>Type</TableCell>
+              <TableCell align="right">Expense Amount</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <div className={classes.seeMore}>
-        <Link color="primary" href="#" onClick={preventDefault}>
-          See more orders
-        </Link>
-      </div>
-    </React.Fragment>
-  );
+          </TableHead>
+          <TableBody>
+            {count.map((row) => (
+              <TableRow key={row.id}>
+                <TableCell>{'16 Mar, 2019'}</TableCell>
+                <TableCell>{row.name}</TableCell>
+                <TableCell>{row.type}</TableCell>
+                <TableCell align="right">£{row.cost}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+        <div className={classes.seeMore}>
+          <Link color="primary" href="#" onClick={preventDefault}>
+            See more orders
+          </Link>
+        </div>
+      </React.Fragment>
+    );
 }
